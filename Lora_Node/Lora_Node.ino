8d43cd537186f7c2d0910da96554405ae38c37be
GitSiddharth2002 <sddhrthsanjay@gmail.com>
2023-05-09 18:37:38 +0530

    transmitter oled display transmiter

--- Lora_Node/Lora_Node.ino
+++ Lora_Node/Lora_Node.ino
@@ -1,11 +1,28 @@
 // Lora Node
 #include <SPI.h>
 #include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 
+
+
+#define i2c_Address 0x3c
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 int counter = 0;
 
 void setup() {
   Serial.begin(115200);
+  delay(250); // wait for the OLED to power up
+  display.begin(i2c_Address, true); // Address 0x3C default
+ 
+  display.display();
+  delay(2000);  
   while (!Serial);
   LoRa.begin(433E6);
 
@@ -18,7 +35,6 @@ void setup() {
 }
 
 void loop() {
-  
   Serial.print("Sending packet: ");
   Serial.println(counter);
 
@@ -26,12 +42,17 @@ void loop() {
   LoRa.beginPacket();
   if (isSmokeDetected()) {
     LoRa.print("Smoke Detected");
-  } else {
+    oled("Smoke Detected",1,10) ;        
+  } 
+  else {
     LoRa.print("Smoke Not Detected");
+    oled("Smoke Not Detected",1,10) ;   
   }
 
   if (flameCount() >= 2) {
-    LoRa.print(" || Flame Detected || "); 
+    LoRa.print(" || Flame Detected || ");
+    oled("Flame Detected",1,20);
+ 
   }
   
   LoRa.print(counter);
